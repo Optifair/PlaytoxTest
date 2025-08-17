e852a7d5a0c7ae6fab315be901c0b42c0cb35e92
opti <optifairpraym@gmail.com>
2025-08-17 21:17:00 +0700

    Account class

--- src/main/java/com/example/model/Account.java
+++ src/main/java/com/example/model/Account.java
@@ -1,4 +1,27 @@
 package com.example.model;
 
+import com.example.exception.InsufficientFundsException;
+
 public class Account {
+    private final String id;
+    private int money;
+
+    public Account(String id, int money) {
+        this.id = id;
+        this.money = money;
+    }
+
+    public synchronized void deposit(int amount) {
+        money += amount;
+    }
+
+    public synchronized void withdraw(int amount) throws InsufficientFundsException {
+        if (money < amount) {
+            throw new InsufficientFundsException("Not enough money on account " + id);
+        }
+        money -= amount;
+    }
+
+    public String getId() { return id; }
+    public int getMoney() { return money; }
 }
